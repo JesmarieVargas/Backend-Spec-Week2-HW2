1ab9e97ff1be3dcb8986e97644f13df338498d06
JesmarieVargas <jesmarie669@gmail.com>
2024-09-08 10:31:26 -0400

    JWT

--- controllers/orderController.py
+++ controllers/orderController.py
@@ -4,21 +4,46 @@ from services import orderService
 from marshmallow import ValidationError
 from cache import cache
 
+from utils.util import admin_required, user_validation
 
-def save(): #name the controller the same as the service it recruites
+
+def save(): 
 
     try:
         order_data = order_schema.load(request.json)
     
     except ValidationError as e:
-        return jsonify(e.messages), 400 #return error message with a 400 failed response
-    
-    new_order = orderService.save(order_data)
-    return order_schema.jsonify(new_order), 201 #send them the customer object with a 201 successful creation status
+        return jsonify(e.messages), 400
+    if token_id == order_data['customer_id']:
+        new_order = orderService.save(order_data)
+        return order_schema.jsonify(new_order), 201
+    else:
+        return jsonify({"message": "You can't order things for other users."}),401
 
 
-@cache.cached(timeout=120)
+# @cache.cached(timeout=120)
 def find_all():
     all_orders = orderService.find_all()
 
     return orders_schema.jsonify(all_orders), 200
+
+def find_by_id(order_id): #dynamic route takes in parameters
+    order = orderService.find_by_id(order_id)
+
+    return order_schema.jsonify(order), 200
+
+@user_validation
+def find_by_customer_id(customer_id, token_id):
+    if customer_id == token_id:
+        orders = orderService.find_by_customer_id(customer_id)
+        return orders_schema.jsonify(orders), 200
+    else:
+        return jsonify({"message": "Cannot view other customers orders!"}), 401
+
+def find_by_email():
+    email = request.json['email']
+    if email:
+        orders = orderService.find_by_email(email)
+        return orders_schema.jsonify(orders), 200
+    else:
+        return jsonify({"message": "Please insert an email"})
--- models/customer.py
+++ models/customer.py
@@ -12,6 +12,7 @@ class Customer(Base):
     phone: Mapped[str] = mapped_column(db.String(25), nullable=False)
     username: Mapped[str] = mapped_column(db.String(30), nullable=False, unique=True)
     password: Mapped[str] = mapped_column(db.String(255), nullable=False)
+    admin: Mapped[int] = mapped_column(db.Integer, nullable=False)
 
     # role: Mapped['Role'] = db.relationship()
     # One-to-Many: Customer and Order
